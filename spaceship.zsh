dada3c27d856f1e4b1f3a5f391c3536ec3ff954d
lefortgu <guillaume.lefort@groupeonepoint.com>
2023-02-03 18:26:58 +0100

    edit spaceship config : spaceship.zsh

--- spaceship.zsh
+++ spaceship.zsh
@@ -14,3 +14,25 @@ spaceship add ember
 # Add a custom vi-mode section to the prompt
 # See: https://github.com/spaceship-prompt/spaceship-vi-mode
 # spaceship add --before char vi_mode
+
+# https://spaceship-prompt.sh/config/intro/#create-a-config-file
+
+SPACESHIP_PROMPT_ASYNC=false
+
+SPACESHIP_GIT_STATUS_MODIFIED='%F{blue} ✘%F{green}'
+SPACESHIP_GIT_STATUS_STASHED="%F{yellow} •%F{green}"
+SPACESHIP_GIT_STATUS_PREFIX='('
+SPACESHIP_GIT_STATUS_SUFFIX=')'
+SPACESHIP_GIT_STATUS_COLOR='green'
+SPACESHIP_GIT_STATUS_BEHIND='%F{red}⇣%F{green}'
+SPACESHIP_GIT_STATUS_DIVERGED='%F{red}⇕%F{green}'
+SPACESHIP_GIT_STATUS_AHEAD='%F{red}⇡%F{green}'
+SPACESHIP_GIT_STATUS_UNMERGED='%F{red}=%F{green}'
+
+SPACESHIP_DOCKER_COMPOSE_SYMBOL="🌊"
+# SPACESHIP_DOCKER_COMPOSE_SUFFIX=''
+# SPACESHIP_PROMPT_DEFAULT_SUFFIX=''
+SPACESHIP_DOCKER_COMPOSE_COLOR_UP=''
+SPACESHIP_DOCKER_COMPOSE_PREFIX=""
+
+SPACESHIP_PACKAGE_SHOW_PRIVATE=true
